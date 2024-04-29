b161dfbde072856f15c01d7174115c111a9da2d3
Jose Quintero Soporte <jose.quintero@macroproyectos.com>
2024-04-29 11:56:21 -0500

    commit update 29/04/2024 header responsive

--- core_frontend/src/app/components/navbar/navbar.component.ts
+++ core_frontend/src/app/components/navbar/navbar.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  menuVisible: boolean = false;
+
+  toggleMenu(event: MouseEvent) {
+    this.menuVisible = !this.menuVisible;
+    event.stopPropagation(); // Para evitar que el evento se propague al hacer clic
+  }
 }
