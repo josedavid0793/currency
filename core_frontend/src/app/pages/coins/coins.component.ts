d708eed0ce922751d43a38e1c2521c6b3ebd05f5
Jose Quintero Soporte <jose.quintero@macroproyectos.com>
2024-05-30 08:37:02 -0500

    commit 30 de mayo paginación a la tabla

--- core_frontend/src/app/pages/coins/coins.component.ts
+++ core_frontend/src/app/pages/coins/coins.component.ts
@@ -8,8 +8,25 @@ import { coinsName } from '../../interfaces/coins';
 })
 export class CoinsComponent {
   coinsName =coinsName;
+  currentPage: number = 1;
+  itemsPerPage: number = 20;
 // Para convertir el objeto en una lista para iterar en la plantilla
 get coinsList() {
   return Object.entries(this.coinsName).map(([code, details]) => ({ code, ...details }));
+}
+get paginatedCoinsList() {
+  const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+  const endIndex = startIndex + this.itemsPerPage;
+  return this.coinsList.slice(startIndex, endIndex);
+}
+
+get totalPages() {
+  return Math.ceil(this.coinsList.length / this.itemsPerPage);
+}
+
+changePage(page: number) {
+  if (page > 0 && page <= this.totalPages) {
+    this.currentPage = page;
+  }
 }
 }
